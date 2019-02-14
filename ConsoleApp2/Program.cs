9ccf07010b159f6d106c959aab418a461559eaaa
oleksandrburinskij <oleksandrburinskij@gmail.com>
2019-02-14 15:31:27 +0200

    update

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,24 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num, dil, rez;
+            num = Convert.ToInt32(Console.ReadLine());
+            int n = 1;
+            while (n <= Math.Sqrt(num))
+            {
+                rez = num % n;
+                dil = num / n;
+                if (rez == 0)
+                {
+                    Console.WriteLine(n);
+                    if (n != dil)
+                    {
+                        Console.WriteLine(dil);
+                    }
+                }
+                n++;
+            }
+            Console.ReadLine();
         }
     }
 }
